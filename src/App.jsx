b144debb87f97c5509e9dea6ce1b718c4ed32523
Brandon Hamilton <bham2015@gmail.com>
2024-09-26 12:46:10 -0500

    Added dummy data to ContactsList.jsx and created state in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import ContactList from './Components/ContactList'
 import './App.css'
 
 function App() {
-
+  const [contacts, setContacts] = useState(dummyContacts);
   return (
     <>
-      
+      <ContactList/>
     </>
   )
 }
